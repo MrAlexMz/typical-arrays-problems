80119f029b85cfdfdd629e5e982f8678a86ce0f3
Alexander Mazalo <mralexmzs@gmail.com>
2021-08-07 15:04:16 +0300

    feat: task completed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || !array.length)
+    return 0;
+  let result = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (result > array[i])
+      result = array[i];
+  }
+  return result;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || !array.length)
+    return 0;
+  let result = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (result < array[i])
+      result = array[i];
+  }
+  return result;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || !array.length)
+    return 0;
+  let result = array[0];
+  for (let i = 1; i < array.length; i++) {
+      result += array[i];
+  }
+  return result / array.length;
 }
